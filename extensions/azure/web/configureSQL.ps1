24c484e2252564875f8003fa65914eeaa3e54068
CMaloun <CMaloun@users.noreply.github.com>
2018-02-28 20:53:09 +0100

    commit

--- extensions/azure/web/configureSQL.ps1
+++ extensions/azure/web/configureSQL.ps1
@@ -117,7 +117,10 @@ if (!($PuppetInstalled)) {
   Write-Host "Environment variable updated"
   
   Restart-Computer -Force
-  Exit 0
+  
 }
 
-
+Set-Location -Path 'C:\Installation SQL SERVER 2014 V1.1\Sources'
+$ScriptToRun= "C:\Installation SQL SERVER 2014 V1.1\Sources\Install_SGBD_Node.ps1"
+&$ScriptToRun -DatacenterId 2 -DomainNameInput "contoso.com" -LoginNameInput "SQLSERVERUSER" -LoginPassword "AweS0me@PW" -SaPassword "If1mdpSQL!" -InstallFailoverCluster "N"
+Exit 0
