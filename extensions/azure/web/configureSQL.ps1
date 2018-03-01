c3eb2d50508f6aac5c10765f4aadc3074d97f0d2
CMaloun <CMaloun@users.noreply.github.com>
2018-03-01 10:52:44 +0100

    commit

--- extensions/azure/web/configureSQL.ps1
+++ extensions/azure/web/configureSQL.ps1
@@ -121,7 +121,11 @@ if (!($PuppetInstalled)) {
 Set-Location -Path 'C:\Installation SQL SERVER 2014 V1.1\Sources'
 $ScriptToRun= ".\Install_SGBD_Node.ps1 -DatacenterId 2 -DomainNameInput 'contoso.com' -LoginNameInput 'SQLSERVERUSER' -LoginPassword 'AweS0me@PW' -SaPassword 'If1mdpSQL!' -InstallFailoverCluster 'N'"
 #&$ScriptToRun -DatacenterId 2 -DomainNameInput "contoso.com" -LoginNameInput "SQLSERVERUSER" -LoginPassword "AweS0me@PW" -SaPassword "If1mdpSQL!" -InstallFailoverCluster "N"
-Start-Process powershell -Verb runAs  $ScriptToRun
+$username = 'contoso\testuser'
+$password = 'AweS0me@PW'
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
+Start-Process powershell -NoNewWindow -Credential $credential $ScriptToRun
 
 #Restart-Computer -Force
 #Exit 0
