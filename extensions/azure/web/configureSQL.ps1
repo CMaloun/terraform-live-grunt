58b3eef251d7966e03402f4184f7b6beab853605
CMaloun <CMaloun@users.noreply.github.com>
2018-03-01 10:53:24 +0100

    commit

--- extensions/azure/web/configureSQL.ps1
+++ extensions/azure/web/configureSQL.ps1
@@ -119,7 +119,7 @@ if (!($PuppetInstalled)) {
 }
 
 Set-Location -Path 'C:\Installation SQL SERVER 2014 V1.1\Sources'
-$ScriptToRun= ".\Install_SGBD_Node.ps1 -DatacenterId 2 -DomainNameInput 'contoso.com' -LoginNameInput 'SQLSERVERUSER' -LoginPassword 'AweS0me@PW' -SaPassword 'If1mdpSQL!' -InstallFailoverCluster 'N'"
+$ScriptToRun= ".\Install_SGBD_Node.ps1 -DatacenterId 2 -DomainNameInput 'contoso.com' -LoginNameInput 'SQLSERVERUSER' -LoginPassword 'AweS0me@PW' -SaPassword 'If1mdpSQL!' -InstallFailoverCluster 'Y'"
 #&$ScriptToRun -DatacenterId 2 -DomainNameInput "contoso.com" -LoginNameInput "SQLSERVERUSER" -LoginPassword "AweS0me@PW" -SaPassword "If1mdpSQL!" -InstallFailoverCluster "N"
 $username = 'contoso\testuser'
 $password = 'AweS0me@PW'
