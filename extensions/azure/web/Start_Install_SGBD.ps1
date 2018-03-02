f5816fbf10474707c2430a85b5f2722059bd297d
Unknown <cmaloungila@gmail.com>
2018-03-02 20:56:28 +0100

    add new file

--- extensions/azure/web/Start_Install_SGBD.ps1
+++ extensions/azure/web/Start_Install_SGBD.ps1
@@ -1,3 +1,7 @@
 Set-Location -Path 'C:\Installation SQL SERVER 2014 V1.1\Sources'
+New-Item 'C:\Installation SQL SERVER 2014 V1.1\Sources\install_done.txt' -ItemType file
 $ScriptToRun = "C:\Installation SQL SERVER 2014 V1.1\Sources\Install_SGBD_Node_2.ps1"
 &$ScriptToRun -DatacenterId 2 -DomainNameInput "contoso.com" -LoginNameInput "SQLSERVERUSER" -LoginPassword "AweS0me@PW" -SaPassword "If1mdpSQL!" -InstallFailoverCluster "Y"
+$ScriptToRunAfter = "C:\Installation SQL SERVER 2014 V1.1\Sources\ChangeUser.ps1"
+$computerName = get-childitem -path env:computername
+&$ScriptToRunAfter -UserName .\LocalSystem -Password 'nothing' -Service 'puppet' -ServiceN $computerName.Value –SecondsToWait 10
