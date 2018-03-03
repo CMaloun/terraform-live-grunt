681c5604872cf886dafe361e13f513fee921a8dc
Unknown <cmaloungila@gmail.com>
2018-03-03 11:54:00 +0100

    Remove the restart

--- extensions/azure/web/Start_Install_SGBD.ps1
+++ extensions/azure/web/Start_Install_SGBD.ps1
@@ -11,4 +11,5 @@ $ScriptToRunAfter = "C:\Installation SQL SERVER 2014 V1.1\Sources\ChangeUser.ps1
 $computerName = get-childitem -path env:computername
 &$ScriptToRunAfter -UserName .\LocalSystem -Password 'nothing' -Service 'puppet' -ServerN $computerName.Value -SecondsToWait 10
 Start-Sleep 20 
-Restart-Computer -Force
+
+#Restart-Computer -Force
