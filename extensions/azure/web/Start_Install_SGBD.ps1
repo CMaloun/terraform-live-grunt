bec94e490436e89dcd524e0b885369f8146b9de5
Unknown <cmaloungila@gmail.com>
2018-03-03 13:21:18 +0100

    add more checks

--- extensions/azure/web/Start_Install_SGBD.ps1
+++ extensions/azure/web/Start_Install_SGBD.ps1
@@ -1,17 +1,18 @@
-TRY {
-    Set-Location -Path 'C:\Installation SQL SERVER 2014 V1.1\Sources'
-    if(![System.IO.File]::Exists('C:\Installation SQL SERVER 2014 V1.1\Sources\install_done.txt')){
-        New-Item 'C:\Installation SQL SERVER 2014 V1.1\Sources\install_done.txt' -ItemType file
+if(![System.IO.File]::Exists('C:\Installation SQL SERVER 2014 V1.1\Sources\install_done.txt')){
+    TRY {
+        Set-Location -Path 'C:\Installation SQL SERVER 2014 V1.1\Sources'
+        if(![System.IO.File]::Exists('C:\Installation SQL SERVER 2014 V1.1\Sources\install_done.txt')){
+            New-Item 'C:\Installation SQL SERVER 2014 V1.1\Sources\install_done.txt' -ItemType file
+        }
+        $ScriptToRun = "C:\Installation SQL SERVER 2014 V1.1\Sources\Install_SGBD_Node_2.ps1"
+        &$ScriptToRun -DatacenterId 2 -DomainNameInput "contoso.com" -LoginNameInput "SQLSERVERUSER" -LoginPassword "AweS0me@PW" -SaPassword "If1mdpSQL!" -InstallFailoverCluster "Y"
     }
-    $ScriptToRun = "C:\Installation SQL SERVER 2014 V1.1\Sources\Install_SGBD_Node_2.ps1"
-    &$ScriptToRun -DatacenterId 2 -DomainNameInput "contoso.com" -LoginNameInput "SQLSERVERUSER" -LoginPassword "AweS0me@PW" -SaPassword "If1mdpSQL!" -InstallFailoverCluster "Y"
+    catch {
+        Write-Host $_.Exception.Message
+    }
+    $ScriptToRunAfter = "C:\Installation SQL SERVER 2014 V1.1\Sources\ChangeUser.ps1"
+    $computerName = get-childitem -path env:computername
+    &$ScriptToRunAfter -UserName .\LocalSystem -Password 'nothing' -Service 'puppet' -ServerN $computerName.Value -SecondsToWait 10
+    Start-Sleep 180
+    Restart-Computer -Force
 }
-catch {
-    Write-Host $_.Exception.Message
-}
-$ScriptToRunAfter = "C:\Installation SQL SERVER 2014 V1.1\Sources\ChangeUser.ps1"
-$computerName = get-childitem -path env:computername
-&$ScriptToRunAfter -UserName .\LocalSystem -Password 'nothing' -Service 'puppet' -ServerN $computerName.Value -SecondsToWait 10
-Start-Sleep 20 
-
-#Restart-Computer -Force
